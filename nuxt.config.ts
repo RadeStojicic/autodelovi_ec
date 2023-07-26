e572a3763dfda30d943671739ba2580aa0e278d7
Rade Stojicic <radestojicicsd@gmail.com>
2023-07-26 22:46:00 +0200

    nuxt-modules-fonts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,23 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
 export default defineNuxtConfig({
-  app: {
-    head: {
-      link: [
-        {
-          rel: "stylesheet",
-          href: "https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900&display=swap",
+    modules: ['@pinia/nuxt', '@nuxtjs/tailwindcss', '@nuxtjs/google-fonts'],
+    googleFonts: {
+        families: {
+            families: {
+                Roboto: true,
+            },
         },
-      ],
     },
-  },
-  modules: ['@pinia/nuxt', '@nuxtjs/tailwindcss'],
-  devtools: { enabled: true },
-  css: ['~/assets/css/main.css'],
-  postcss: {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
+
+    devtools: { enabled: true },
+    css: ['~/assets/css/main.css'],
+    postcss: {
+        plugins: {
+            tailwindcss: {},
+            autoprefixer: {},
+        },
     },
-  },
 })
